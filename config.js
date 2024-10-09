aeb9d3f53ac9f18f691c833812d54911c51eef52
linconnors <linconnrs211@gmail.com>
2024-10-09 08:32:31 -0700

    adding files for WildRydes

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_JXjUybvGI', // Replace with your User Pool ID
+        userPoolClientId: 'IDl4uo8ubl34d4pe8dq86bfc4mequ', // Replace with your App Client ID
+        region: 'us-east-1' // Replace with your AWS region code
+    },
+    api: {
+        invokeUrl: '' // Optional: Your API endpoint
+    }
+};
