76159b4a5b6a17e8128c8b7ca52ec04a77b593ec
linconnors <linconnrs211@gmail.com>
2024-10-12 15:14:04 -0700

    Fix config.js values

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-us-west-1_D1tustMru37o8mt6ft02hv8tqe2o5l7o7j2window._config = {
+window._config = {
     cognito: {
-        userPoolId: 'us-west-1_D1tustMru', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '37o8mt6ft02hv8tqe2o5l7o7j2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_JXjUybvGI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4uo8ubl34d4pe8dq86bfc4mequ', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
